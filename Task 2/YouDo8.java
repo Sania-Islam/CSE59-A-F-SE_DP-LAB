fcaa9f97ca5023ac1264f911330f0b68d7b2b115
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-09 11:24:10 +0600

    Fix typo in total cost calculation
    Corrected typo in the printBill method for total cost.

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,39 @@
+class Item{
+    String name;
+    double price;
+
+    public Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+}
+
+
+class Order{
+    Item item1;
+    Item item2;
+
+    public  Order(Item item1, Item item2){
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+
+public void printBill() {
+        double totalCost = item1.price + item2.price;
+        System.out.println("--- Your Bill ---");
+        System.out.println(item1.name + ": $" + item1.price);
+        System.out.println(item2.name + ": $" + item2.price);
+        System.out.println("Total Cost: $" + totalCost); // টাইপো totalaCost ঠিক করা হয়েছে
+    }
+}
+public class Main8 {
+    public static void main(String[] args){
+        Item mouse = new Item("Mouse", 20.0);
+        Item keyboard = new Item("Keyboard", 45.0);
+    
+        Order myOrder = new Order(mouse, keyboard);
+
+        myOrder.printBill();
+    }
+}
 
