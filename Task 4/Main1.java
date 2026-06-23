65cceb62d37447b1a61055f319a5ef4de4772adc
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-23 09:59:44 +0600

    Update Main1.java

--- Task 4/Main1.java
+++ Task 4/Main1.java
@@ -1,1 +1,70 @@
+public interface Shape
+{
+    double calculateArea();
+}
 
+public class Rectangle implements Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea(){
+        return length * width;
+    }
+}
+
+public class Circle implements Shape{
+    private double radious;
+    public Circle(double radious){
+        this.radious = radious;
+    }
+
+    @Override
+    public double calculateArea() {
+        
+        return  Math.PI * (radious * radious);
+    }
+}
+
+public class AreaCalculator {
+
+    public double calculateTotalArea(Shape[] shapes){
+        double totalArea = 0;
+        for(Shape shape : shapes){
+            totalArea += shape.calculateArea();
+        }
+        return totalArea;
+    }
+}
+public class Triangle implements Shape{
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height){
+        this.base =- base;
+        this.height = height;
+    }
+
+    public double calculateArea(){
+        return 0.5 * base * height;
+    }
+}
+
+public class Main1 {
+public static void main(String[] args){
+    Shape[] shapes = new Shape[]{
+        new Rectangle(5, 4),
+        new Circle(3),
+        new Triangle(6, 4)
+    };
+
+    AreaCalculator calculator = new AreaCalculator();
+    double totalArea = calculator.calculateTotalArea(shapes);
+
+    System.out.println("Total Area: " + totalArea);
+}
+}
