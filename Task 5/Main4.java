01830e7a8e6cf7e8a37ad46f9c38e2f689480d5e
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-30 10:45:11 +0600

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,33 @@
+interface OptimizableModel{
+     void quantize();
+}
+
+
+class ResNet18 implements OptimizableModel{
+    public void quantize(){
+        System.out.println("Applying int8 quantization for ResNet-18.");
+    }
+}
+
+class MobileNet implements OptimizableModel{
+    public void quantize(){
+        System.out.println("Applying dynaimic quyantization");
+    }
+}
+
+class InferenceEngine {
+    public void runOptimization(OptimizableModel model) {
+        model.quantize();
+    }
+}
+public class main4 {
+    public static void main(String[] args) {
+        InferenceEngine engine = new InferenceEngine();
+        OptimizableModel resnet = new ResNet18();
+        OptimizableModel mobileNet = new MobileNet();
+
+        engine.runOptimization(resnet);
+        engine.runOptimization(mobileNet);
+    }
+}
 
