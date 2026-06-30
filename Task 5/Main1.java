b60076bd9f818c6df7f651fd15fe33fd3a9787d7
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-30 09:47:47 +0600

    Update Main1.java

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,46 @@
+interface Printer {
+void printDocument();
+    
+}
 
+interface Scanner{
+    void scanDocument();
+}
+
+interface FaxMachine{
+    void faxDocument();
+}
+
+class    MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+    public void printDocument(){
+        System.out.println("Printing.........");
+    }
+
+    public void scanDocument(){
+        System.out.println("Scanning....");
+    }
+    public void faxDocument(){
+        System.out.println("Faxing....");
+    }
+}
+
+class BasicPrinter implements Printer {
+    public void printDocument() {
+        System.out.println("Print......");
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Printer basicPrinter = new BasicPrinter();
+
+        basicPrinter.printDocument();
+
+        System.out.println();
+
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
