067036749bd596312df9ac681ec21ae37f1d6045
Halima Saida Sania <155908601+Sania-Islam@users.noreply.github.com>
2026-06-30 10:57:19 +0600

    Update Main5.java

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,40 @@
+class UniversityMember {
+    public void attendLecture(){
+        System.out.println("Attending a lecture.");
+    }
+}
+
+interface Researcher {
+    void conductLabResearch();
+}
+
+class UndergraduateStudent extends UniversityMember {
+
+}
+
+class GraduateStudent extends UniversityMember implements Researcher {
+    public void conductLabResearch(){
+        System.out.println("Graduate student doing research.");
+    }
+}
+
+class Professor extends UniversityMember implements Researcher {
+    public void conductLabResearch() {
+        System.out.println("Professor leading research.");
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        UniversityMember u1 = new UniversityMember();
+        u1.attendLecture();
+
+        Researcher r1 = new GraduateStudent();
+        Researcher r2 = new Professor();
+
+        r1.conductLabResearch();
+        r2.conductLabResearch();
+    }
+    
+}
 
